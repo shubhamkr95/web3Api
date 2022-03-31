cc64a052cd3e93c982a348b095ebbec204fe142f
shubhamkr95 <sahooshubham124@gmail.com>
2022-03-31 20:23:16 +0530

    added utils dir appError.js catchAsync.js contract.js email, nodeUrl and transaction email file

---
+++ utils/contract.js
@@ -0,0 +1,114 @@
+require('dotenv').config();
+const { ethers } = require('ethers');
+const { URL } = require('./nodeUrl');
+
+const customHttpProvider = new ethers.providers.JsonRpcProvider(URL);
+
+// CONTRACT ADDRESS ROPSTEN
+// const contractAddress = '0xba871f6b449529d2ebef5beb9f8d51968b9919d3';
+
+// Local blockchain contract address
+const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+
+const contractABI = [
+  {
+    inputs: [],
+    stateMutability: 'nonpayable',
+    type: 'constructor',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'account',
+        type: 'address',
+      },
+    ],
+    name: 'balanceOf',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'name',
+    outputs: [
+      {
+        internalType: 'string',
+        name: '',
+        type: 'string',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'owner',
+    outputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'symbol',
+    outputs: [
+      {
+        internalType: 'string',
+        name: '',
+        type: 'string',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'totalSupply',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'to',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'amount',
+        type: 'uint256',
+      },
+    ],
+    name: 'transfer',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+];
+
+exports.tokenContract = new ethers.Contract(
+  contractAddress,
+  contractABI,
+  customHttpProvider
+);
---
+++ utils/nodeUrl.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+// Ropsten url
+// module.exports.URL = process.env.ROPSTEN_API_KEY;
+
+// local node url
+module.exports.URL = 'http://127.0.0.1:8545';
