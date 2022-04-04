79f69fc740f1614421a0e34aff043a53d2853fc7
shubhamkr95 <sahooshubham124@gmail.com>
2022-04-04 14:36:01 +0530

    modified server file

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express';
 import logger from './config/logger.js';
+import dbConnect from './connections/database.js';
 
 const app = express();
 app.use(express.json());
-
-const authRoutes = require('./routes/auth');
-const allUser = require('./routes/user');
-const transaction = require('./routes/transaction');
-
-app.use('/wallet/', authRoutes);
-app.use('/wallet/', allUser);
-app.use('/wallet/', transaction);
 
 const port = process.env.port || 3000;
 app.listen(port, () => {
