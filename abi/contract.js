62172fd199c4b6a064edf64eeb68315589b4adc4
shubhamkr95 <sahooshubham124@gmail.com>
2022-04-04 16:23:27 +0530

    added provider,address and  abi to contract.js

---
+++ abi/contract.js
@@ -0,0 +1,116 @@
+import 'dotenv/config';
+import ethers from 'ethers';
+import URL from '../utils/nodeUrl.js';
+
+const customHttpProvider = new ethers.providers.JsonRpcProvider(URL);
+
+// CONTRACT ADDRESS ROPSTEN
+// const contractAddress = '0xba871f6b449529d2ebef5beb9f8d51968b9919d3';
+
+// Local blockchain contract address
+const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+
+const contractABI = [
+  {
+    inputs: [],
+    stateMutability: 'nonpayable',
+    type: 'constructor',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'account',
+        type: 'address',
+      },
+    ],
+    name: 'balanceOf',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'name',
+    outputs: [
+      {
+        internalType: 'string',
+        name: '',
+        type: 'string',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'owner',
+    outputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'symbol',
+    outputs: [
+      {
+        internalType: 'string',
+        name: '',
+        type: 'string',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'totalSupply',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'to',
+        type: 'address',
+      },
+      {
+        internalType: 'uint256',
+        name: 'amount',
+        type: 'uint256',
+      },
+    ],
+    name: 'transfer',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+];
+
+const tokenContract = new ethers.Contract(
+  contractAddress,
+  contractABI,
+  customHttpProvider
+);
+
+export default tokenContract;
