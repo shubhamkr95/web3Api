6c5dd8f59b2c9bf80001896c63bbc3da55c5530d
shubhamkr95 <sahooshubham124@gmail.com>
2022-04-04 17:02:18 +0530

    added user section to user.js

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,27 @@
 import Users from '../models/user.js';
 import catchAsync from '../utils/catchAsync.js';
 import { handleSuccess } from '../helpers/responseHandler.js';
+
+// Get current users
+const user = catchAsync(async (req, res, next) => {
+  const user = await Users.findById(req.user.id);
+
+  // check weather user is admin or not
+  if (user.role === 'admin') {
+    const allUsers = await Users.find();
+    handleSuccess({
+      res,
+      msg: `Welcome admin`,
+      data: allUsers,
+    });
+  } else {
+    const currUser = user;
+    handleSuccess({
+      res,
+      msg: `Welcome user`,
+      data: currUser,
+    });
+  }
+});
+
+export default user;
