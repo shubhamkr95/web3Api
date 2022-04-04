999fdd867dbe11b16a335f0c7c49328fad49e046
shubhamkr95 <sahooshubham124@gmail.com>
2022-04-04 15:49:15 +0530

    added errror route handler

--- server.js
+++ server.js
@@ -2,10 +2,20 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import logger from './config/logger.js';
+import { AppError, handleError } from './helpers/responseHandler.js';
 import dbConnect from './connections/database.js';
 
 const app = express();
 app.use(express.json());
+
+app.all('*', (req, res, next) => {
+  next(
+    handleError({
+      res,
+      data: `Cannot find ${req.originalUrl} on this server!`,
+    })
+  );
+});
 
 const port = process.env.port || 3000;
 app.listen(port, () => {
